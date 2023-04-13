8aad0a7bd4405913edcd76de3aea8f0f103a96eb
Lauren Ingraham <laureningy@gmail.com>
2023-04-13 17:06:20 -0400

    Bot Set Up

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SSW345ChessBot
 SSW 345 Final Project - Discord Chess Bot
+
+
+# Set Up:
+
+# Windows
+py -3 -m pip install -U discord.py
+
+py -3 -m pip install python-dotenv
+
+# To Run:
+python main.py
---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+import os
+#import random
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
