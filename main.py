0b01e5a197d9c76490845a75e6d22697b5831e41
cjrkoa <76755988+cjrkoa@users.noreply.github.com>
2023-04-14 12:44:46 -0400

    Bot Command
    Simple bot command

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import discord
 import os
 #import random
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+# default permissions + message_content (lets us send messages)
+intents = discord.Intents.default()
+intents.message_content= True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# bot wrapper for commands
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# put this before every bot command a user can put into the chat 
+# ctx (context) is always the first argument, arg (or something like: arg1, arg2 or args*) is user input
+@bot.command()
+async def hello(ctx, arg):
+    print("Sending Message...")
+    await ctx.send("Hello " + arg)
+    print("Message Sent")
+
+bot.run(TOKEN)
 client.run(TOKEN)
