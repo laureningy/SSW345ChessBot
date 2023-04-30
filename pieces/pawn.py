cb84a80871108314e4dd326f991c4382fdd7e2b2
Lauren Ingraham <laureningy@gmail.com>
2023-04-30 19:33:24 -0400

    added docstrings to methods

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -8,13 +8,26 @@ class Pawn(Piece):
         self.name = 'Pawn'
 
     def name(self):
+        '''
+        returns the name of the piece
+        '''
         return self.name
     
     def square(self):
+        '''
+        returns the current board square that the piece is on
+        '''
         return self.current_square
     
     def move(self, square):
+        '''
+        moves the piece to a new square on the board
+        updates current_square to input square if the move is legal
+        '''
         pass
 
     def isValidMove(self, square):
+        '''
+        returns boolean of whether a move is logically valid
+        '''
         return False
